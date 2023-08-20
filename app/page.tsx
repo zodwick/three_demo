4d5be5cb861488ead0305b22a91805977bc81cc1
zodwick <115868723+zodwick@users.noreply.github.com>
2023-08-20 16:59:32 +0530

    addition

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Model from './model'
+
+export default function page() {
+  return (
+    <>
+  <div className="  w-full   h-screen">
+
+  <Model  />
+
+  </div>
+        
+   
+      
+     
+    </>
+
+  )
+}
